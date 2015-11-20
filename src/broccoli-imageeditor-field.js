561a1c5b52a45fac3c39baffe9dc58e195c695b5
Misaki Shibata <misaki.pink@gmail.com>
2015-11-20 15:24:08 +0900

    gulpで自動生成されるファイル

--- src/broccoli-imageeditor-field.js
+++
@@ -1,3 +0,0 @@
-(function(window){
-	window.BroccoliImageEditorField = require('../libs/client.js');
-})(window);
--- tests/testdata/htdocs/libs/broccoli-psd-field.js
+++ tests/testdata/htdocs/libs/broccoli-psd-field.js
@@ -1333,8 +1333,8 @@ module.exports = (function () {
 
   return false;
 })();
-}).call(this,require("DF1urx"))
-},{"DF1urx":3}],18:[function(require,module,exports){
+}).call(this,require("1YiZ5S"))
+},{"1YiZ5S":3}],18:[function(require,module,exports){
 "use strict";
 
 module.exports = asString
@@ -14788,90 +14788,8 @@ module.exports = phpjs;
 
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./build/npm":20}],22:[function(require,module,exports){
-// console.log(broccoli);
+(function(window){
+	window.BroccoliPSDField = require('../libs/client.js');
+})(window);
 
-/**
- * main.js
- */
-window.main = new (function(){
-	var _this = this;
-	var it79 = require('iterate79');
-	var socket = this.socket = window.baobabFw
-		.createSocket(
-			this,
-			io,
-			{
-				'showSocketTest': function( data, callback, main, socket ){
-					// console.log(data);
-					// alert(data.message);
-					// console.log(callback);
-					callback(data);
-					return;
-				}
-			}
-		)
-	;
-
-	// broccoli をインスタンス化
-	var broccoli = new Broccoli();
-	this.broccoli = window.broccoli = broccoli;
-
-	this.init = function(callback){
-		callback = callback||function(){};
-		// this.socketTest();
-		// broccoli を初期化
-		broccoli.init(
-			{
-				'elmCanvas': $('.canvas').get(0),
-				'elmModulePalette': $('.palette').get(0),
-				'contents_area_selector': '[data-contents]',
-				'contents_bowl_name_by': 'data-contents',
-				'customFields': {
-					'psd': require('./../../../../libs/client.js')
-				},
-				'gpiBridge': function(api, options, callback){
-					// General Purpose Interface Bridge
-					socket.send(
-						'broccoli',
-						{
-							'api': 'gpiBridge' ,
-							'bridge': {
-								'api': api ,
-								'options': options
-							}
-						} ,
-						function(rtn){
-							// console.log(rtn);
-							callback(rtn);
-						}
-					);
-					return;
-				}
-			} ,
-			function(){
-				$(window).resize(function(){
-					broccoli.redraw();
-				});
-				callback();
-			}
-		);
-	}
-
-	/**
-	 * WebSocket疎通確認
-	 */
-	this.socketTest = function(){
-		socket.send(
-			'socketTest',
-			{'message': 'socketTest from frontend.'} ,
-			function(data){
-				console.log(data);
-				// alert('callback function is called!');
-			}
-		);
-		return this;
-	}
-
-})();
-
-},{"./../../../../libs/client.js":1,"iterate79":4}]},{},[22])
+},{"../libs/client.js":1}]},{},[22])
